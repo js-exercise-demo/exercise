d28b05d6375501bac5b26919b65898307638bfd4
liyanyan19940914 <921258351@qq.com>
2017-09-09 22:27:32 +0800

    revify booking

---
+++ spec/badminton-court-spec.js
@@ -0,0 +1,15 @@
+describe('Take out food', function () {
+
+  it('should generate best charge when no promotion can be used', function() {
+    let inputs = ["ITEM0013 x 4"];
+    let summary = bestCharge(inputs).trim();
+    let expected = `
+============= 订餐明细 =============
+肉夹馍 x 4 = 24元
+-----------------------------------
+总计：24元
+===================================`.trim()
+    expect(summary).toEqual(expected)
+  });
+
+});
